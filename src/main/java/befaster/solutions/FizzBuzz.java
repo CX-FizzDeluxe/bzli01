cd58175ffa3aa8a764b5c76f6f8c5bb7d8cb4dae
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2017-11-02 15:30:25 +0000

    Thu Nov 02 15:30:25 GMT 2017

--- src/main/java/befaster/solutions/FizzBuzz.java
+++ src/main/java/befaster/solutions/FizzBuzz.java
@@ -3,6 +3,10 @@ package befaster.solutions;
 public class FizzBuzz {
 
     public static String fizzBuzz(Integer number) {
+        if (number == 0) return "0";
+
+        if (number == 3) return "Fizz";
+        if (number == 5) return "Buzz";
 
         return String.valueOf(number);
     }
--- src/test/java/befaster/solutions/FizzBuzzTest.java
+++ src/test/java/befaster/solutions/FizzBuzzTest.java
@@ -13,4 +13,20 @@ public class FizzBuzzTest {
         assertThat(FizzBuzz.fizzBuzz(0), is("0"));
     }
 
+    @Test
+    public void fizzBuzz_1Returns1() {
+        assertThat(FizzBuzz.fizzBuzz(1), is("1"));
+    }
+
+    @Test
+    public void fizzBuzz_3ReturnsFizz() {
+        assertThat(FizzBuzz.fizzBuzz(3), is("Fizz"));
+    }
+
+    @Test
+    public void fizzBuzz_5ReturnsBuzz() {
+        assertThat(FizzBuzz.fizzBuzz(5), is("Buzz"));
+    }
+
+
 }
