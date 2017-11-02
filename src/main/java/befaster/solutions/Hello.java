206704c4f42a70ee832838342d321f2ab4ac08f6
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2017-11-02 15:23:11 +0000

    Thu Nov 02 15:23:11 GMT 2017

--- src/main/java/befaster/solutions/Hello.java
+++ src/main/java/befaster/solutions/Hello.java
@@ -2,6 +2,8 @@ package befaster.solutions;
 
 public class Hello {
     public static String hello(String friendName) {
-        return String.format("Hello, %s!", friendName);
+
+        return "Hello, World!";
+//        return String.format("Hello, %s!", friendName);
     }
 }
