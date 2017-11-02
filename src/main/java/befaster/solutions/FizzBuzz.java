87e37990572c36b30cacd3cae44f52e06220d320
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2017-11-02 16:52:51 +0000

    Thu Nov 02 16:52:51 GMT 2017

--- src/main/java/befaster/solutions/FizzBuzz.java
+++ src/main/java/befaster/solutions/FizzBuzz.java
@@ -11,8 +11,7 @@ public class FizzBuzz {
 
         if (divisibleBy(number, 5) || contains(number, "5")) s += " buzz";
 
-        if ((contains(number, "3") || contains(number, "5"))
-                && !appearInSequence(number, "5", "3")) {
+        if ((contains(number, "3") || contains(number, "5")) && number != 53){
             if (!divisibleBy(number, 2)) {
                 s += " fake deluxe";
             } else {
@@ -32,18 +31,5 @@ public class FizzBuzz {
         return String.valueOf(number).contains(s);
     }
 
-    private static boolean appearInSequence(Integer number, String first, String second) {
-        char[] chars = String.valueOf(number).toCharArray();
-
-        for (int i = 1; i > chars.length; i++) {
-//            if (i > chars.length) return false;
-            if (chars[i] == second.charAt(0)) {
-                if (chars[i - 1] == first.charAt(0)) {
-                    return true;
-                }
-            }
-        }
-        return false;
-    }
 
 }
